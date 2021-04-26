55d18fd2f8b1d9bf462e3d602e6b6999352d1ded
Andrey Gromov <digital.designs.russia@gmail.com>
2021-04-26 16:29:49 +0300

    3.3. Загрузка файлов на github [#243562]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args){
+        System.out.println("Andrey Gromov");
+        System.out.println("02.04.1983");
+    }
+}
