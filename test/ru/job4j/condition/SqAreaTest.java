1106479ae34941f440458e39adc9fb6efe8fa7e8
JavAndy58 <digital.designs.russia@gmail.com>
2021-05-01 14:24:56 +0300

    8. Стороны прямоугольника. [#137735 #107]

--- test/ru/job4j/condition/SqAreaTest.java
+++ test/ru/job4j/condition/SqAreaTest.java
@@ -23,12 +23,4 @@ public class SqAreaTest {
         Assert.assertEquals(expected, out, 0.01);
     }
 
-    @Test
-    public void whenP12K2Square8() {
-        int expected = 8;
-        int p = 12;
-        double k = 2;
-        double out = SqArea.square(p, k);
-        Assert.assertEquals(expected, out, 0.01);
-    }
 }
