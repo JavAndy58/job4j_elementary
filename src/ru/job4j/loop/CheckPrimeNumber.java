30c50b0ac7aee724082688ae700360e1156b528c
JavAndy58 <digital.designs.russia@gmail.com>
2021-05-06 20:18:40 +0300

    5.9. Крест в псевдографике. [#177182]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,21 +2,18 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = number > 1;
+        boolean prime = true;
 
-            for (int i = 2; i < number; i++) {
-                if (number % i == 0) {
-                    prime = false;
-                    break;
-                }
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0 || number == 1) {
+                prime = false;
+                break;
             }
-
+        }
         return prime;
     }
 
     public static void main(String[] args) {
-        System.out.println(check(5));
-        System.out.println(check(6));
         System.out.println(check(1));
     }
 }
